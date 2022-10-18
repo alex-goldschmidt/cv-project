2faca1c8c887c8de6b2ce1f20c4029b614696210
alex-goldschmidt <83550948+alex-goldschmidt@users.noreply.github.com>
2022-10-17 23:57:00 -0400

    Create ability to add personal info

---
+++ src/components/Personal.js
@@ -0,0 +1,78 @@
+import { useState } from "react";
+import styles from "../styles/styles.css";
+
+let PersonalId = 0;
+const Personal = () => {
+  const [Value, setValue] = useState("");
+  //“setName” function sets state variable "Name". useState("") call initializes variable "Name" with an empty string.
+  const [PersonalData, setPersonalData] = useState([]);
+  //“setPersonalData” function sets state variable "PersonalData". useState([]) call initializes variable "PersonalData" with an empty array.
+  const NameInput = document.querySelector(".NameInput");
+  const EmailInput = document.querySelector(".EmailInput");
+  const NumberInput = document.querySelector(".NumberInput");
+  return (
+    <div className="PersonalContainer">
+      <form
+        className="NameForm"
+        value={Value}
+        onChange={(e) => setValue(e.target.value)}
+        onSubmit={(e) => {
+          e.preventDefault();
+          setPersonalData([
+            ...PersonalData,
+            { id: PersonalId++, Value: Value },
+          ]); // create new array with old items and 1 new item at the end.
+          NameInput.style.display = "none";
+        }}
+      >
+        <div className="NameInput">
+          <input placeholder="Name" /> <button>Add Name</button>
+        </div>
+      </form>
+
+      <form
+        className="EmailForm"
+        value={Value}
+        onChange={(e) => setValue(e.target.value)}
+        onSubmit={(e) => {
+          e.preventDefault();
+          setPersonalData([
+            ...PersonalData,
+            { id: PersonalId++, Value: Value },
+          ]); // create new array with old items and 1 new item at the end.
+          EmailInput.style.display = "none";
+        }}
+      >
+        <div className="EmailInput">
+          <input placeholder="Email" /> <button>Add Email</button>
+        </div>
+      </form>
+
+      <form
+        className="NumberForm"
+        value={Value}
+        onChange={(e) => setValue(e.target.value)}
+        onSubmit={(e) => {
+          e.preventDefault();
+          setPersonalData([
+            ...PersonalData,
+            { id: PersonalId++, Value: Value },
+          ]); // create new array with old items and 1 new item at the end.
+          NumberInput.style.display = "none";
+        }}
+      >
+        <div className="NumberInput">
+          <input placeholder="Phone Number" /> <button>Add Phone Number</button>
+        </div>
+      </form>
+
+      <div className="cvContainer">
+        {PersonalData.map((data) => (
+          <li key={data.id}>{data.Value} </li>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Personal;
